64cdf2d678d82a1694d8d262f35d312153902b1c
Almar Klein <almar.klein@gmail.com>
2017-06-04 23:39:41 +0200

    Haha, a brainfuck to WASM compiler!

--- brainfuck/brainfuck.py
+++ brainfuck/brainfuck.py
@@ -52,23 +52,79 @@ Pointer :   ^
 >++.                    And finally a newline from Cell #6
 """
 
-# TODO: this is a work in progress. It needs WASM linear memory and I have not looked at that very well yet
+
+def _commands2instructions(commands):
+    """ Compile brainfuck commands to WASM instructions (as tuples).
+    """
+    # The i32.load8_u instruction takes one values from the stack (the address),
+    # and i32.store8 and takes two (address and value). Both instructions take
+    # two arguments, 2**alignment (i.e. 0 means 1 byte alignment) and address offset.
+    
+    instructions = []
+    while commands:
+        c = commands.pop(0)
+        if c == '>':
+            instructions += [('get_local', 0), ('i32.const', 1), ('i32.add'), ('set_local', 0)]
+        elif c == '<':
+            instructions += [('get_local', 0), ('i32.const', 1), ('i32.sub'), ('set_local', 0)]
+        elif c == '+':
+            instructions += [('get_local', 0), ('get_local', 0),  # once for the read, once for the write
+                             ('i32.load8_u', 0, 0),
+                             ('i32.const', 1), ('i32.add'), ('i32.store8', 0, 0)]
+        elif c == '-':
+            instructions += [('get_local', 0), ('get_local', 0),  # once for the read, once for the write
+                             ('i32.load8_u', 0, 0),
+                             ('i32.const', 1), ('i32.sub'), ('i32.store8', 0, 0)]
+        elif c == '.':
+            instructions += [('get_local', 0), ('i32.load8_u', 0, 0), ('call', 0)]
+        elif c == ',':
+            # We don't support input, just set to zero
+            instructions += [('get_local', 0), ('i32.const', 0), ('i32.store8', 0, 0)]
+        elif c == '[':
+            instructions += [('block', 'emptyblock'),
+                                # if current data point == 0 goto end of block
+                                ('get_local', 0), ('i32.load8_u', 0, 0), ('i32.const', 0), ('i32.eq'), ('br_if', 0),
+                                ('loop', 'emptyblock'),
+                                    ] + _commands2instructions(commands ) + [
+                                    # if current data point > 0 goto start of block
+                                    ('get_local', 0), ('i32.load8_u', 0, 0), ('i32.const', 0), ('i32.ne'), ('br_if', 0),
+                                ('end'),
+                             ('end')]
+        elif c == ']':
+            break
+        else:
+            raise ValueError('Unknown Brainfuck command: %r' % c)
+    
+    return instructions
+
 
 def brainfuck2wasm(code):
-    commands = [c for c in code if c in '><+-.,[]'
+    """ Compile brainfuck code to a WASM module.
+    """
+    commands = [c for c in code if c in '><+-.,[]']
     
+    instructions = _commands2instructions(commands)
     
     module = Module(
         TypeSection(
             FunctionSig([]),  # start func
-            FunctionSig(['f64']),  # import write func
+            FunctionSig(['i32']),  # imported func to write chars
             ),
         ImportSection(
-            Import('js', 'stdout_write', 'function', 1),
+            Import('js', 'stdout_write_char', 'function', 1),
             ),
         FunctionSection(0),
+        MemorySection((1, 1)),  # 1 page of 64KiB > 30000 minimum for Brainfuck
         StartSection(1),
         CodeSection(
-            FunctionDef([], *instructions))
+            FunctionDef(['i32'], *instructions)),
+        DataSection(),  # no initial data
         )
     return module
+
+
+if __name__ == '__main__':
+    wasm = brainfuck2wasm(EXAMPLE)
+    print('nbytes:', len(wasm.to_binary()))
+    insert_wasm_into_html(__file__[:-3] + '.html', wasm.to_binary())
+    
