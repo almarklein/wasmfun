f10be916a9ce94a5baffb7da4e9848c7c00478a2
Almar Klein <almar.klein@gmail.com>
2017-07-16 23:32:33 +0200

    dist with flit

--- wasmfun/__init__.py
+++ wasmfun/__init__.py
@@ -3,6 +3,8 @@ A Python library that provides tools to handle WASM code, like generating
 WASM, and perhaps someday interpreting it too.
 """
 
+__version__ = '0.1'
+
 from ._opcodes import OPCODES
 from .fields import *
 from .util import *
